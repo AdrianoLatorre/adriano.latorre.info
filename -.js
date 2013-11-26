874e0c1b04904398e626032173ce71440354c948
Adriano Latorre <adriano@latorre.info>
2013-11-26 10:20:41 +0100

    Bugs and Firefox and things

--- -.js
+++ -.js
@@ -1,23 +1,36 @@
 function boot() {
     $('#txt a').click(function () {
         $('#txt a').removeClass('selected');
-        $(this).addClass('selected');
+        $(this).addClass('selected').css('background-position', '-500px 0');
         hide();
     });
     $('#webframe').load(function () {
-        show();
+        if(pb) {
+            show();
+        }
     });
 }
 
+var pb;
+
 function hide() {
-    $('#container').removeClass('show').addClass('hide');
-    $('#website').removeClass('show').addClass('hide');
+    $('#container, #website').removeClass('show').addClass('hide');
+    clearInterval(pb);
+    pb = setInterval('startbar()', 250);
 }
 
 function show() {
-    if ($('#container').hasClass('hide')) {
-        $('#container').removeClass('hide').addClass('show');
-        $('#website').removeClass('hide').addClass('show');
+    $('#container, #website').removeClass('hide').addClass('show');
+    stopbar();
+}
+
+function startbar() {
+    if(parseFloat($('a.selected').css('background-position'))<-250) {
+        $('a.selected').css({backgroundPosition: function( index, value ) {return parseFloat(value)+(Math.floor((Math.random()*10)+1));}});
     }
 }
 
+function stopbar() {
+    clearInterval(pb);
+    $('a.selected').css('background-position', '-50px 0');
+}
